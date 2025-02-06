e45e56378817a4e6691ae72e1e2a7cdb46019654
resendecode <gabrielresende02@proton.me>
2025-02-06 22:34:01 +0100

    remise à niveau du projet niveau front

--- socio-pulse-front/src/app/navbar/navbar.component.ts
+++ socio-pulse-front/src/app/navbar/navbar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    MatToolbar,
+    RouterLink
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
