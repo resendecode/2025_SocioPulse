6fce9b0c0bec2f77b29efbc4ddc3ee5b4628f1b0
resendecode <gabrielresende02@proton.me>
2025-02-09 11:17:18 +0100

    changement de nom

--- 2025_SocioPulse-develop-front/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {LandingPageComponent} from './landing-page/landing-page.component';
-import {RegisterPageComponent} from './register-page/register-page.component';
-
-export const routes: Routes = [
-  {path:'', component: LandingPageComponent},
-  {path: 'register', component: RegisterPageComponent}
-];
